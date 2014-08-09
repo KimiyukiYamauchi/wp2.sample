b1bc41802d3fb4ab2951be9c252dfeb7fb97f827
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-08-09 11:09:49 +0900

    ページ番号を表示

--- pagenation.php
+++ pagenation.php
@@ -1,4 +1,19 @@
-<p class="pagenation">
-<span class="oldpage"><?php next_posts_link('&laquo; 古い記事'); ?></span>
-<span class="newpage"><?php previous_posts_link('新しい記事 &raquo;'); ?></span<>
+<p class="pagenation pagenum">
+
+<?php 
+$maxpage = $wp_query->max_num_pages;
+$current = $paged;
+if(!$current){$current = 1;}
+?>
+
+<?php previous_posts_link('&lt;'); ?>
+<?php for($i=1; $i<=$maxpage; $i++): ?>
+	<?php if($i == $current): ?>
+	<span><?php echo $i; ?></span>
+	<?php else: ?>
+	<a href="<?php echo get_pagenum_link($i); ?>"><?php echo $i; ?></a>
+	<?php endif; ?>
+<?php endfor; ?>
+<?php next_posts_link('&gt;'); ?>
+
 </p>
